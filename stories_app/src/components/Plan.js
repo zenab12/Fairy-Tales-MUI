350d4f5eaa74c3a8dc31f4e82bd2801676a0fbd5
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-04-17 04:54:37 +0200

    Update Plan.js

--- stories_app/src/components/Plan.js
+++ stories_app/src/components/Plan.js
@@ -71,7 +71,7 @@ const Plan = () => {
                     {
                         plans_content.map(
                             (item, index) =>
-                                <Grid key={index} item xs={10} md={5} lg={3} sx={{ alignSelf: 'center' }}>
+                                <Grid key={index} item xs={12} md={5} lg={3} sx={{ alignSelf: 'center' }}>
                                     <Card variant='outlined'>
                                         <Box sx={{ display: 'flex', flexDirection: 'column', p: '20px 0px' }}>
                                             <CardContent sx={{ p: 0, flex: '1 0 auto', display: 'flex', flexDirection: 'column', gap: 3, textAlign: 'center' }}>
